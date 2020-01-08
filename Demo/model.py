7fe539a93b985b51a22d79d026b6e08fb24101d2
hacors <hakaorson@outlook.com>
2020-01-08 14:25:38 +0000

    temp

--- Demo/model.py
+++ Demo/model.py
@@ -1,20 +1,25 @@
 from sklearn import metrics
 from torch.utils.data.dataset import Dataset
+from torch.utils.data.dataloader import DataLoader
 import preprocess
 import torch
 from torch import nn
 
 
-class csv_dataset(Dataset):
+class Mol_dataset(Dataset):
     def __init__(self, data):
         self.data = data
 
     def __getitem__(self, index):
-        pass
+        return self.data[index]
 
     def __len__(self):
         return len(self.data)
 
+
+Mol_dataloader = DataLoader(Mol_dataset, batch_size=3, shuffle=True)
+for temp in Mol_dataloader:
+    print(temp)
 
 loss_func = nn.BCEWithLogitsLoss()
 metric_func = metrics.roc_auc_score()
